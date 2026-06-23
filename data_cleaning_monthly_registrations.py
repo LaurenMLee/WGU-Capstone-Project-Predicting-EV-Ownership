8e5960879ab8f8f7ee7fb755e58f44cce0d9d200
LaurenMLee <lee.lauren.m@gmail.com>
2026-06-22 20:55:46 -0400

    Cleaning Registration Data Outline

---
+++ data_cleaning_monthly_registrations.py
@@ -0,0 +1,14 @@
+# Load data
+
+
+# Cleanup column names
+
+# Cleanup values in fuel category column
+
+# Display starting rows
+
+# Combine total EV counts by month/year and zipcode
+
+# Save Data
+
+# Display Results
