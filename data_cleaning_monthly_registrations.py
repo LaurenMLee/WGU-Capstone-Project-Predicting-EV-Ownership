5d690cae9d0b666fe8f0b9e6617c20d5978ef9ec
LaurenMLee <lee.lauren.m@gmail.com>
2026-06-23 11:41:24 -0400

    Updated data cleaning to add all possible md zipcodes in the registrations csv

--- data_cleaning_monthly_registrations.py
+++ data_cleaning_monthly_registrations.py
@@ -12,9 +12,9 @@ def clean_monthly_registrations_data():
     # Cleanup column names to remove empty spaces at front or end of string'
     monthly_registration_data.columns = monthly_registration_data.columns.str.strip()
 
-    # Remove rows with missing values
+    # Remove rows with missing values in date/fuel category or zip code columns
     monthly_registration_data = monthly_registration_data.dropna(
-        subset=["Year_Month","Fuel_Category","Zip_Code", "Count"]
+        subset=["Year_Month","Fuel_Category","Zip_Code"]
     )
 
     # Display  rows after deleting rows with missing values
@@ -56,19 +56,62 @@ def clean_monthly_registrations_data():
     print("Rows after combining fuel categories", len(monthly_registration_data))
 
 
-    # Add City/ County Names to zip codes
 
 
-
-    # Load Zip Code Mapping Crosswalk
+    # Load Zip Code Mapping Crosswalk to add City/County Names and to serve as dataset for all MD Zip Codes that exist
     zip_code_mapping = pd.read_csv("data/processed_data/zipcode_mapping_clean.csv",
         dtype={"Zip_Code": str})
 
-    monthly_registration_data = monthly_registration_data.merge(
-        zip_code_mapping[["Zip_Code", "City", "County"]],
-        on="Zip_Code",
-        how="inner"
+    # Create complete list of all months in the registration data without duplicates and sort them
+    all_months = monthly_registration_data["Month_Registered"].drop_duplicates().sort_values()
+    all_months = pd.DataFrame({"Month_Registered": all_months})
+
+    print("Number of months:", len(all_months))
+    print("Number of Maryland ZIP codes:", len(zip_code_mapping))
+
+    all_zip_month_combos = all_months.merge(
+        zip_code_mapping,
+        how="cross"
     )
+    # Display row count after adding in all MD Zip Codes
+    print("Rows after creating all month/ZIP combinations:", len(all_zip_month_combos))
+
+    # Fill in data for months where registration data exists
+
+    monthly_registration_data = all_zip_month_combos.merge(
+        monthly_registration_data[
+            ["Month_Registered", "Zip_Code", "Total_EV_Registrations"]
+        ],
+        on=["Month_Registered", "Zip_Code"],
+        how="left"
+    )
+
+    # Add 0 for all months that a zipcode has no new registrations
+    monthly_registration_data["Total_EV_Registrations"] = (
+        monthly_registration_data["Total_EV_Registrations"]
+        .fillna(0)
+        .astype(int)
+    )
+    # Display count of rows that have no registrations in a given month
+
+    print(
+        "Rows with 0 registrations in Month:",
+        (monthly_registration_data["Total_EV_Registrations"] == 0).sum()
+    )
+    # Display count of rows that have at least 1 registration in a given month
+    print(
+        "Rows with 1 or more registrations in Month",
+        (monthly_registration_data["Total_EV_Registrations"] > 0).sum()
+    )
+    # Arrange Column Order
+    monthly_registration_data = monthly_registration_data[
+        ["Month_Registered", "Zip_Code", "City", "County", "Total_EV_Registrations"]
+    ]
+
+    # Sort by date first, then zip code
+    monthly_registration_data = monthly_registration_data.sort_values(
+        ["Month_Registered", "Zip_Code"]
+    ).reset_index(drop=True)
 
     # Save Data
 
@@ -76,7 +119,7 @@ def clean_monthly_registrations_data():
 
     # Display Results
     print("Monthly Registration Cleaning Complete")
-    print("Rows after cleaning completion:", len(monthly_registration_data))
+    print("Rows after Cleaning Completion:", len(monthly_registration_data))
 
     return monthly_registration_data
 
